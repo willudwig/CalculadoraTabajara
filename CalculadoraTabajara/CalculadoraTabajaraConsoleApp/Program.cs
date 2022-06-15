c9666a4bdf9370e109bb8c150758ef59803a225f
William Ludwig <acesso.william@gmail.com>
2022-06-15 15:48:17 -0300

    AdicionadoProjeto_CaculadorTabajaraConsoleapp

---
+++ CalculadoraTabajara/CalculadoraTabajaraConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CalculadoraTabajaraConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
