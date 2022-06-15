63d1a7d89243365878888a8cd1dd76135095965d
William Ludwig <acesso.william@gmail.com>
2022-06-15 16:02:40 -0300

    Adicionado codigo base_CalculadoraTabaja

--- CalculadoraTabajara/CalculadoraTabajaraConsoleApp/Program.cs
+++ CalculadoraTabajara/CalculadoraTabajaraConsoleApp/Program.cs
@@ -2,12 +2,81 @@
 
 namespace CalculadoraTabajaraConsoleApp
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opcao;
 
+            while (true)
+            {
+                #region Menu Principal
+
+                Console.Clear();
+
+                Console.WriteLine("Calculadora Tabajara\n");
+
+                Console.WriteLine("Tela Principal\n");
+
+                Console.WriteLine("Digite S para sair\n");
+
+                Console.Write("Opção: ");
+                opcao = Console.ReadLine();
+
+                if (opcao.Equals("s", StringComparison.OrdinalIgnoreCase))
+                    break;
+
+                #endregion
+
+                #region Realizar Calculo
+
+                Console.Clear();
+
+                Console.WriteLine("Calculadora Tabajara\n");
+
+                string operacao = "";
+
+                switch (opcao)
+                {
+                    default:
+                        break;
+                }
+
+                string subtitulo = $"Novo calculo de {operacao}";
+
+                Console.WriteLine(subtitulo + "\n");
+
+                Console.Write($"Digite o primeiro número: ");
+                double primeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write($"Digite o segundo número: ");
+                double segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+                double resultado = 0;
+
+                switch (opcao)
+                {
+                    default:
+                        break;
+                }
+                #endregion
+
+                #region Mostrar Resultado
+                Console.Clear();
+
+                Console.WriteLine("Calculadora Tabajara\n");
+
+                Console.WriteLine("Tela de Resultados\n");
+
+                Console.Write($"O resultado da operação de {operacao} é: {resultado}");
+
+                Console.WriteLine();
+
+                Console.ReadLine();
+
+                Console.Clear();
+                #endregion
+            }
         }
     }
 }
